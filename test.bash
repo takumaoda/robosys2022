867980ba0c961f345edc9e6b245dc7e4143a28f9
Takuma Oda <legotakuma@icloud.com>
2022-11-28 01:07:23 +0900

    improvement

--- test.bash
+++ test.bash
@@ -1,2 +1,18 @@
+#! /bin/bash
+# SPDX-FileCopyrightText: 2022 Takuma Oda
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+ng (){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ]
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK		# &&は左側が成功すると右側を実行
+exit $res
